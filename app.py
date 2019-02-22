243460e209ba7b7660b9e9f5d6d2186af4f0daa3
box_chang <box_chang@mail.eagleskytech.com>
2019-02-22 16:04:53 +0800

    init

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def handle_message(event):
         TextSendMessage(text=event.message.text))
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=7777)
+    app.run(host='127.0.0.1', port=5000)
